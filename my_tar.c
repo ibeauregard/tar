97d03c66d7fe6c940688df522c38edfaa757aa40
Ian Beauregard <ian.beauregard@gmail.com>
2021-01-27 12:26:43 -0500

    Skeleton app

---
+++ my_tar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int my_tar(int n_arguments, char** arguments)
+{
+	printf("Called my_tar with %d arguments:\n", n_arguments);
+	for (int i = 0; i < n_arguments; i++) {
+		puts(arguments[i]);
+	}
+	return 0;
+}
+
+int main(int argc, char** argv)
+{
+	return my_tar(argc - 1, argv + 1);
+}
