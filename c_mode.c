b96e425f7de8d58f081ef3c70894b2c710ccb163
Ian Beauregard <ian.beauregard@gmail.com>
2021-01-28 18:27:43 -0500

    Preliminary structure for c mode

--- c_mode.c
+++ c_mode.c
@@ -1,7 +1,21 @@
 #include "modes.h"
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+#define ARCHIVE_FLAGS O_CREAT|O_WRONLY|O_TRUNC
+#define ARCHIVE_MODE S_IRUSR|S_IWUSR|S_IRGRP|S_IROTH
 
 int c_mode(Params *params)
 {
-	params->archivePath = 0;
+	int archiveFD = open(params->archivePath, ARCHIVE_FLAGS, ARCHIVE_MODE);
+	PathNode *pathNode = params->filePaths;
+	while (pathNode) {
+//		append(archiveFD, pathNode->path);
+		PathNode *current = pathNode;
+		pathNode = pathNode->next;
+		free(current);
+	}
+	close(archiveFD);
 	return 0;
 }
