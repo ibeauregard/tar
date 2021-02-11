79b7eb834c4f3971fc4cf499ca5a1c249f2014ea
Ian Beauregard <ian.beauregard@gmail.com>
2021-02-10 20:28:42 -0500

    Update -r and -u modes for new interface

--- src/r_mode.c
+++ src/r_mode.c
@@ -10,17 +10,15 @@ int r_mode(Params *params)
 	return sanityCheck(params->archivePath) || create(params, true, NULL);
 }
 
-// TODO: should not always return EXIT_SUCCESS
 int sanityCheck(char *archivePath)
 {
-	// TODO: Ask for interface change to TarNode *parseTar(char *archivePath, int *status);...
-	// TODO: ... or int parseTar(char *archivePath, TarNode **headNode);
-	TarNode *headNode = parseTar(archivePath);
+	int status;
+	TarNode *headNode = parseTar(archivePath, &status);
 	while (headNode) {
 		TarNode *current = headNode;
 		free(current->header);
 		headNode = current->next;
 		free(current);
 	}
-	return EXIT_SUCCESS;
+	return status;
 }
