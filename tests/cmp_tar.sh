c668733ec92b380af4ec373644ae549cfc732a88
dj <>
2021-02-11 19:28:36 -0500

    Changed test script to not compare trailing null bytes. Also refactored and cleaned up some code.

--- tests/cmp_tar.sh
+++ tests/cmp_tar.sh
@@ -7,8 +7,7 @@ fi
 
 # Delete intermediate files before script ends
 cleanUp() {
-	for arg in ${BASH_ARGV[*]}
-	do
+	for arg in "$@"; do
 		rm $arg.txt >> /dev/null 2>&1
 		rm my_$arg.txt >> /dev/null 2>&1
 		rm $arg.tar >> /dev/null 2>&1
@@ -21,121 +20,137 @@ cleanUp() {
 }
 
 # TEST 1: Creating tar archive
-for arg in $@
-do
-	name=arg
-	tar -f $arg.tar -c $arg
-	hexdump -C $arg.tar > $arg.txt
-	../my_tar -f my_$arg.tar -c $arg
-	hexdump -C my_$arg.tar > my_$arg.tar
-	res=$(cmp -b $arg.txt my_$arg.txt) 
-	status=$?
-	if [ $status -gt 0 ]; then
-		printf "FAILED TEST 1: Creating tar archive\n"
-		echo $res
-		cleanUp
-		exit 1
-	fi
-done
-printf "PASSED TEST 1: Creating tar archive\n"
-
-# TEST 2: Listing tar contents 
-for arg in $@
-do
-	name=arg
-	tar -tf $arg.tar > $arg.txt
-	../my_tar -tf $arg.tar > my_$arg.txt
-	res=$(cmp -b $arg.txt my_$arg.txt) 
-	status=$?
-	if [ $status -gt 0 ]; then
-		printf "FAILED TEST 2: Listing tar contents\n"
-		echo $res
-		cleanUp
-		exit 1
-	fi
-done
-printf "PASSED TEST 2: Listing tar contents\n"
-
-# TEST 3: Append tar contents
-for arg in $@
-do
-	name=arg
-	tar -f $arg.tar -c $arg
-	tar -rf $arg.tar emptytest filetest dirtest
-	../my_tar -cf my_$arg.tar $arg
-	../my_tar -rf my_$arg.tar emptytest filetest dirtest
-	res=$(cmp -b $arg.tar my_$arg.tar) 
-	status=$?
-	if [ $status -gt 0 ]; then
-		printf "FAILED TEST 3: Append tar contents\n"
-		echo $res
-		cleanUp
-		exit 1
-	fi
-done
-printf "PASSED TEST 3: Append tar contents\n"
-
-# TEST 4: Update tar contents
-for arg in $@
-do
-	name=arg
-	tar -uf $arg.tar u_flag/emptytest u_flag/filetest
-	../my_tar -uf $arg.tar u_flag/emptytest u_flag/filetest
-	res=$(cmp -b $arg.tar my_$arg.tar) 
-	status=$?
-	if [ $status -gt 0 ]; then
-		printf "FAILED TEST 4: Update tar contents\n"
-		echo $res
-		cleanUp
-		exit 1
-	fi
-done
-printf "PASSED TEST 4: Update tar contents\n"
-
-# TEST 5: Extract tar contents
-for arg in $@
-do
-	name=arg
-	# Extract contents using tar
-	mkdir extract_tar >> /dev/null 2>&1
-	cd extract_tar
-	tar -xf ../$arg.tar 
-	cd ..
-
-	# Extract contents using my_tar
-	mkdir extract_my_tar >> /dev/null 2>&1
-	cd extract_my_tar
-	../../my_tar -xf ../my_$arg.tar 
-	cd ..
-
-	# Ensure contents of directories are the same
-	cd extract_tar
-	find * -type f -exec echo "{}" \; > ../extract_tar_contents.txt
-	cd ../extract_my_tar
-	find * -type f -exec echo "{}" \; > ../extract_my_tar_contents.txt
-	cd ..
-	res=$(cmp -b extract_tar_contents.txt extract_my_tar_contents.txt) 
-	status=$?
-	if [ $status -gt 0 ]; then
-		printf "FAILED TEST 5: Extract tar contents (missing files)\n"
-		echo $res
-		cleanUp
-		exit 1
-	fi
-
-	# Ensure contents of files are the same
-	while read pathname;
-	do 
-		res=$(cmp -b extract_tar/$pathname extract_my_tar/$pathname) 
+testCMode() {
+	for arg in "$@"
+	do
+		name=arg
+		tar -f $arg.tar -c $arg
+		hexdump -C $arg.tar > $arg.txt
+		../my_tar -f my_$arg.tar -c $arg
+		hexdump -C my_$arg.tar > my_$arg.txt
+		res=$(cmp -b <(head -n -1 $arg.txt) <(head -n -1 my_$arg.txt))
+		status=$?
 		if [ $status -gt 0 ]; then
-			printf "FAILED TEST 5: Extract tar contents (bad contents)\n"
+			printf "FAILED TEST 1: Creating tar archive\n"
 			echo $res
-			cleanUp
+			cleanUp "$@"
 			exit 1
 		fi
-	done < extract_tar_contents.txt
-done
-printf "PASSED TEST 5: Extract tar contents\n"
+	done
+	printf "PASSED TEST 1: Creating tar archive\n"
+}
 
-cleanUp
+# TEST 2: Append tar contents
+testRMode() {
+	for arg in $@
+	do
+		name=arg
+		tar -f $arg.tar -c $arg
+		tar -rf $arg.tar emptytest filetest dirtest
+		../my_tar -cf my_$arg.tar $arg
+		../my_tar -rf my_$arg.tar emptytest filetest dirtest
+		res=$(cmp -b $arg.tar my_$arg.tar) 
+		status=$?
+		if [ $status -gt 0 ]; then
+			printf "FAILED TEST 2: Append tar contents\n"
+			echo $res
+			cleanUp "$@"
+			exit 1
+		fi
+	done
+	printf "PASSED TEST 2: Append tar contents\n"
+}
+
+# TEST 3: Update tar contents
+testUMode() {
+	for arg in $@
+	do
+		name=arg
+		tar -uf $arg.tar u_flag/emptytest u_flag/filetest
+		../my_tar -uf $arg.tar u_flag/emptytest u_flag/filetest
+		res=$(cmp -b $arg.tar my_$arg.tar) 
+		status=$?
+		if [ $status -gt 0 ]; then
+			printf "FAILED TEST 3: Update tar contents\n"
+			echo $res
+			cleanUp "$@"
+			exit 1
+		fi
+	done
+	printf "PASSED TEST 3: Update tar contents\n"
+}
+
+# TEST 4: Listing tar contents 
+testTMode() {
+	for arg in $@
+	do
+		name=arg
+		tar -tf $arg.tar > $arg.txt
+		../my_tar -tf $arg.tar > my_$arg.txt
+		res=$(cmp -b $arg.txt my_$arg.txt) 
+		status=$?
+		if [ $status -gt 0 ]; then
+			printf "FAILED TEST 4: Listing tar contents\n"
+			echo $res
+			cleanUp "$@"
+			exit 1
+		fi
+	done
+	printf "PASSED TEST 4: Listing tar contents\n"
+}
+
+# TEST 5: Extract tar contents
+testXMode() {
+	for arg in $@
+	do
+		name=arg
+		# Extract contents using tar
+		mkdir extract_tar >> /dev/null 2>&1
+		cd extract_tar
+		tar -xf ../$arg.tar 
+		cd ..
+
+		# Extract contents using my_tar
+		mkdir extract_my_tar >> /dev/null 2>&1
+		cd extract_my_tar
+		../../my_tar -xf ../my_$arg.tar 
+		cd ..
+
+		# Ensure contents of directories are the same
+		cd extract_tar
+		find * -type f -exec echo "{}" \; > ../extract_tar_contents.txt
+		cd ../extract_my_tar
+		find * -type f -exec echo "{}" \; > ../extract_my_tar_contents.txt
+		cd ..
+		res=$(cmp -b extract_tar_contents.txt extract_my_tar_contents.txt) 
+		status=$?
+		if [ $status -gt 0 ]; then
+			printf "FAILED TEST 5: Extract tar (missing files)\n"
+			echo $res
+			cleanUp "$@"
+			exit 1
+		fi
+
+		# Ensure contents of files are the same
+		while read pathname;
+		do 
+			res=$(cmp -b extract_tar/$pathname extract_my_tar/$pathname) 
+			status=$?
+			if [ $status -gt 0 ]; then
+				printf "FAILED TEST 5: Extract tar (bad contents)\n"
+				echo $res
+				cleanUp "$@"
+				exit 1
+			fi
+		done < extract_tar_contents.txt
+	done
+	printf "PASSED TEST 5: Extract tar contents\n"
+}
+
+testCMode "$@"
+# testRMode "$@"
+# testUMode "$@"
+# testTMode "$@"
+testXMode "$@"
+cleanUp "$@"
 exit 0 
